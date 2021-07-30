cf0d6c09d13a49b46683eb348180494a9dc403bd
chris <christian.r.lie@hotmail.no>
2021-07-30 21:55:29 +0200

    Quiz done

---
+++ frontend/src/test/java/org/tsdes/intro/exercises/quizgame/selenium/po/LayoutPO.java
@@ -0,0 +1,42 @@
+package org.tsdes.intro.exercises.quizgame.selenium.po;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.tsdes.intro.exercises.quizgame.selenium.PageObject;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public abstract class LayoutPO extends PageObject {
+    public LayoutPO(WebDriver driver, String host, int port) {
+        super(driver, host, port);
+    }
+
+    public LayoutPO(PageObject other) {
+        super(other);
+    }
+
+    public SignUpPO toSignUp(){
+
+        clickAndWait("linkToSignupId");
+
+        SignUpPO po = new SignUpPO(this);
+        assertTrue(po.isOnPage());
+
+        return po;
+    }
+
+    public IndexPO doLogout(){
+
+        clickAndWait("logoutId");
+
+        IndexPO po = new IndexPO(this);
+        assertTrue(po.isOnPage());
+
+        return po;
+    }
+
+    public boolean isLoggedIn(){
+
+        return getDriver().findElements(By.id("logoutId")).size() > 0 &&
+                getDriver().findElements((By.id("linkToSignupId"))).isEmpty();
+    }
+}
